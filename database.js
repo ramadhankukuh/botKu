b19d70bdbfeea3413bfb60fce79ee188fe78063c
Kuh. <73922679+ramadhankukuh@users.noreply.github.com>
2025-06-08 21:19:27 +0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ MongoDB Connected');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err.message);
+  }
+}
+
+module.exports = connectDB;
